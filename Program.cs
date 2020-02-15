1246435c7bfd5da96b1e4a1b8647f93dfca015eb
Sharun <sharunksplus@gmail.com>
2020-02-15 12:44:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace terminal
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Diagnostics.Process.Start("wt.exe", "-d .");
+        }
+    }
+}
